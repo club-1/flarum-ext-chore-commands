5bf5ed60ecd1d7aaaae7268807a701e83d0c796b
Nicolas Peugnet <nicolas@club1.fr>
2023-04-26 18:46:10 +0200

    initial commit, working chore:parse command

---
+++ README.md
@@ -0,0 +1,34 @@
+# Chore Commands
+
+![License](https://img.shields.io/badge/license-AGPL--3.0--or--later-blue.svg) [![Latest Stable Version](https://img.shields.io/packagist/v/club-1/flarum-ext-chore-commands.svg)](https://packagist.org/packages/club-1/flarum-ext-chore-commands) [![Total Downloads](https://img.shields.io/packagist/dt/club-1/flarum-ext-chore-commands.svg)](https://packagist.org/packages/club-1/flarum-ext-chore-commands)
+
+A [Flarum](http://flarum.org) extension. Adds a few maintenance commands to Flarum console.
+
+Once enabled in the admin dashboard, it provides the following Flarum console commands:
+
+```
+Available commands for the "chore" namespace:
+  chore:reparse  Reparse all comment posts using the latest formatter's configuration
+```
+
+## Installation
+
+Install with composer:
+
+```sh
+composer require club-1/flarum-ext-chore-commands:"*"
+```
+
+## Updating
+
+```sh
+composer update club-1/flarum-ext-chore-commands:"*"
+```
+
+## Links
+
+- [Packagist](https://packagist.org/packages/club-1/flarum-ext-chore-commands)
+- [GitHub](https://github.com/club-1/flarum-ext-chore-commands)
+<!--
+- [Discuss](https://discuss.flarum.org/d/PUT_DISCUSS_SLUG_HERE)
+-->
---
+++ src/Console/ReparseCommand.php
@@ -0,0 +1,76 @@
+<?php
+
+/*
+ * This file is part of club-1/flarum-ext-chore-commands.
+ *
+ * Copyright (c) 2023 Nicolas Peugnet <nicolas@club1.fr>.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+namespace Club1\ChoreCommands\Console;
+
+use Flarum\Console\AbstractCommand;
+use Flarum\Formatter\Formatter;
+use Flarum\Post\CommentPost;
+use Flarum\Post\Post;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+class ReparseCommand extends AbstractCommand
+{
+    const CHUNK_SIKE = 100;
+
+    /** @var Formatter */
+    protected $formatter;
+
+    public function __construct(Formatter $formatter) {
+        parent::__construct();
+        $this->formatter = $formatter;
+        // Flush formatter to be sure to have the latest configuration
+        $this->formatter->flush();
+    }
+
+    protected function configure()
+    {
+        $this
+            ->setName('chore:reparse')
+            ->setDescription('Reparse all comment posts using the latest formatter\'s configuration')
+            ->addOption('yes', 'y', InputOption::VALUE_NONE, 'Reply "yes" to all questions');
+    }
+
+    protected function fire()
+    {
+        $in = $this->input;
+        $io = new SymfonyStyle($in, $this->output);
+        $io->warning('This will reparse all the comment posts in the database with the latest formatter\'s configuration');
+        if (!$in->getOption('yes') && !$io->confirm('Do you want to continue?', false)) {
+            return static::FAILURE;
+        }
+        $posts = Post::query()
+            ->where('type', 'comment')
+            ->select(['id', 'content', 'user_id', 'edited_user_id'])
+            ->lazyById(static::CHUNK_SIKE);
+        $posts = $io->progressIterate($posts);
+        foreach ($posts as $post) {
+            assert($post instanceof CommentPost);
+            $src = $this->formatter->unparse($post->content, $post);
+            $user = is_null($post->editedUser) ? $post->user : $post->editedUser;
+            $post->content = $this->formatter->parse($src, $post, $user);
+            $post->save();
+        }
+    }
+}
